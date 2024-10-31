709aced53ec15c9baadb922fba60bb68b7ee1028
Jeremy Rose <nornagon@nornagon.net>
2024-10-31 12:05:59 -0700

    better uniqueSize

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,3 @@
-import type { IModule } from "dependency-cruiser";
-import { HTMLAttributes } from "preact/compat";
 import { useEffect, useMemo, useState } from "preact/hooks";
 
 function Size({ size }: { size: number }) {
@@ -11,45 +9,61 @@ function Size({ size }: { size: number }) {
   return <span>{formattedSize} KB</span>;
 }
 
-function isValid(module: {
-  coreModule?: boolean;
-  dependencyTypes?: string | string[];
-}) {
-  return !module.coreModule;
-}
+const modulesMapCache = new WeakMap<ModuleInfo[], Map<string, ModuleInfo>>();
 
-const modulesMapCache = new WeakMap<IModule[], Map<string, IModule>>();
-
-function findModule(modules: IModule[], source: string) {
+function findModule(modules: ModuleInfo[], id: string) {
   if (!modulesMapCache.has(modules)) {
-    const modulesMap = new Map<string, IModule>(
-      modules.map((m) => [m.source, m])
+    const modulesMap = new Map<string, ModuleInfo>(
+      modules.map((m) => [m.id, m])
     );
     modulesMapCache.set(modules, modulesMap);
   }
-  return modulesMapCache.get(modules)!.get(source);
+  return modulesMapCache.get(modules)!.get(id);
 }
 
 function Module({
   modules,
-  module,
-  entryPoint = module,
+  path,
+  entryPoint = path[path.length - 1],
   hoveredModule,
   setHoveredModule,
 }: {
-  modules: IModule[];
-  module: IModule;
-  entryPoint?: IModule;
-  hoveredModule: IModule | null;
-  setHoveredModule: (module: IModule | null) => void;
+  modules: ModuleInfo[];
+  path: ModuleInfo[];
+  entryPoint?: ModuleInfo;
+  hoveredModule: ModuleInfo | null;
+  setHoveredModule: (module: ModuleInfo | null) => void;
 }) {
+  const module = path[path.length - 1];
   const [isOpen, setIsOpen] = useState(false);
   const isDepOfHovered =
     hoveredModule != null &&
-    allTransitiveDependencies(modules, hoveredModule).has(module.source);
+    allTransitiveDependencies(modules, hoveredModule).has(module.id);
   const hoveredIsDepOfThis =
     hoveredModule != null &&
-    allTransitiveDependencies(modules, module).has(hoveredModule.source);
+    allTransitiveDependencies(modules, module).has(hoveredModule.id);
+  const isHovered = hoveredModule?.id === module.id;
+
+  function showId(id: string) {
+    if (/node_modules/.test(id)) {
+      const nodeModulesIndex = id.lastIndexOf("node_modules");
+      if (nodeModulesIndex === -1) {
+        return id;
+      }
+      const parts = id.slice(nodeModulesIndex).split("/");
+      const scopedPackage = parts[1].startsWith("@");
+      const packageName = scopedPackage
+        ? parts.slice(1, 3).join("/")
+        : parts[1];
+      return (
+        <>
+          <span style={{ fontWeight: "bold" }}>{packageName}</span>
+          {`/${parts.slice(scopedPackage ? 3 : 2).join("/")}`}
+        </>
+      );
+    }
+    return id;
+  }
   return (
     <li>
       <details open={isOpen}>
@@ -61,37 +75,38 @@ function Module({
           onMouseEnter={() => setHoveredModule(module)}
           onMouseLeave={() => setHoveredModule(null)}
           style={{
-            color: isDepOfHovered
+            color: isHovered
               ? "red"
-              : hoveredIsDepOfThis
+              : isDepOfHovered
               ? "blue"
+              : hoveredIsDepOfThis
+              ? "green"
               : "inherit",
           }}
         >
-          {module.source} (self <Size size={module.minifiedSize} />, total{" "}
+          {showId(module.id)} (self <Size size={module.size} />, total{" "}
           <Size size={totalSize(modules, module)} />, unique{" "}
-          <Size size={uniqueSize(modules, entryPoint, module)} />)
+          <Size size={uniqueSize(modules, entryPoint, path)} />)
         </summary>
         {isOpen && (
           <ul>
-            {module.dependencies
-              .filter((dep) => !dep.dynamic && isValid(dep))
+            {module.importedIds
               .toSorted((a, b) => {
-                const aModule = findModule(modules, a.resolved);
-                const bModule = findModule(modules, b.resolved);
+                const aModule = findModule(modules, a);
+                const bModule = findModule(modules, b);
                 if (aModule == null || bModule == null) {
                   return 0;
                 }
                 return (
-                  uniqueSize(modules, entryPoint, bModule) -
-                  uniqueSize(modules, entryPoint, aModule)
+                  uniqueSize(modules, entryPoint, [...path, bModule]) -
+                  uniqueSize(modules, entryPoint, [...path, aModule])
                 );
               })
               .map((dep) => (
-                <li key={dep.resolved}>
+                <li key={dep}>
                   <Module
                     modules={modules}
-                    module={findModule(modules, dep.resolved)!}
+                    path={[...path, findModule(modules, dep)!]}
                     entryPoint={entryPoint}
                     hoveredModule={hoveredModule}
                     setHoveredModule={setHoveredModule}
@@ -105,32 +120,47 @@ function Module({
   );
 }
 
+type ModuleInfo = {
+  id: string;
+  size: number;
+  importedIds: string[];
+  dynamicallyImportedIds: string[];
+};
+
+function importers(modules: ModuleInfo[], module: ModuleInfo) {
+  return modules.filter(
+    (m) =>
+      m.importedIds.includes(module.id) ||
+      m.dynamicallyImportedIds.includes(module.id)
+  );
+}
+
 export function App() {
-  const [modules, setData] = useState<IModule[] | null>(null);
+  const [modules, setData] = useState<ModuleInfo[] | null>(null);
   useEffect(() => {
-    fetch("./depcruise.json")
+    fetch("./bundle-analyzer.json")
       .then((res) => res.json())
       .then((data) =>
         setData(
-          (data as IModule[]).toSorted(
+          (data as ModuleInfo[]).toSorted(
             (a, b) => totalSize(data, b) - totalSize(data, a)
           )
         )
       );
   }, []);
   const entryPoints = useMemo(() => {
-    return modules?.filter((module) => module.dependents.length === 0);
+    return modules?.filter((module) => importers(modules, module).length === 0);
   }, [modules]);
-  const [hoveredModule, setHoveredModule] = useState<IModule | null>(null);
+  const [hoveredModule, setHoveredModule] = useState<ModuleInfo | null>(null);
 
   return (
     modules != null && (
       <ul>
         {entryPoints?.map((module) => (
           <Module
-            key={module.source}
+            key={module.id}
             modules={modules}
-            module={module}
+            path={[module]}
             setHoveredModule={setHoveredModule}
             hoveredModule={hoveredModule}
           />
@@ -142,121 +172,111 @@ export function App() {
 
 const totalSizeCache = new Map<string, number>();
 
-function totalSize(modules: IModule[], module: IModule) {
-  if (totalSizeCache.has(module.source)) {
-    return totalSizeCache.get(module.source)!;
+function totalSize(modules: ModuleInfo[], module: ModuleInfo) {
+  if (totalSizeCache.has(module.id)) {
+    return totalSizeCache.get(module.id)!;
   }
-  const self = module.minifiedSize;
+  const self = module.size;
   const deps = allTransitiveDependencies(modules, module);
   let total = self;
   for (const dep of deps) {
     const depModule = findModule(modules, dep);
     if (depModule != null) {
-      total += depModule.minifiedSize;
+      total += depModule.size;
     }
   }
-  totalSizeCache.set(module.source, total);
+  totalSizeCache.set(module.id, total);
   return total;
 }
 
 const transitiveDependenciesCache = new Map<string, Set<string>>();
 
 function allTransitiveDependencies(
-  modules: IModule[],
-  module: IModule
+  modules: ModuleInfo[],
+  module: ModuleInfo,
+  visited = new Set<string>()
 ): Set<string> {
-  if (transitiveDependenciesCache.has(module.source)) {
-    return transitiveDependenciesCache.get(module.source)!;
+  if (transitiveDependenciesCache.has(module.id)) {
+    return transitiveDependenciesCache.get(module.id)!;
   }
 
+  if (visited.has(module.id)) {
+    return new Set(); // Handle circular dependencies by returning an empty set
+  }
+
+  visited.add(module.id);
+
   const deps = new Set<string>();
-  for (const dep of module.dependencies) {
-    if (dep.dynamic || dep.coreModule) {
-      continue;
-    }
-    const depModule = findModule(modules, dep.resolved);
+  for (const dep of module.importedIds) {
+    const depModule = findModule(modules, dep);
     if (depModule != null) {
-      deps.add(depModule.source);
+      deps.add(depModule.id);
       for (const transitiveDep of allTransitiveDependencies(
         modules,
-        depModule
+        depModule,
+        visited
       )) {
         deps.add(transitiveDep);
       }
     }
   }
 
-  transitiveDependenciesCache.set(module.source, deps);
+  transitiveDependenciesCache.set(module.id, deps);
   return deps;
 }
 
 const uniqueSizeCache = new Map<string, number>();
 
-function uniqueSize(
-  modules: IModule[],
-  entryPoint: IModule,
-  chokePoint: IModule
+function* reachableFrom(
+  modules: ModuleInfo[],
+  entryPoint: ModuleInfo,
+  condition: (path: ModuleInfo[]) => boolean = () => true
 ) {
-  const cacheKey = `${entryPoint.source}-${chokePoint.source}`;
-  if (uniqueSizeCache.has(cacheKey)) {
-    return uniqueSizeCache.get(cacheKey)!;
-  }
-  if (entryPoint.source === chokePoint.source) {
-    return totalSize(modules, chokePoint);
-  }
-
-  const modulesMap = new Map<string, IModule>(
-    modules.map((m) => [m.source, m])
-  );
-  const deps = allTransitiveDependencies(modules, chokePoint);
-  let total = chokePoint.minifiedSize;
-  for (const dep of deps) {
-    const depModule = modulesMap.get(dep);
-    if (
-      depModule != null &&
-      !reachableFromWithout(modules, entryPoint, depModule, chokePoint)
-    ) {
-      total += depModule.minifiedSize;
-    }
-  }
-  uniqueSizeCache.set(cacheKey, total);
-  return total;
-}
-function reachableFromWithout(
-  modules: IModule[],
-  entryPoint: IModule,
-  target: IModule,
-  avoid: IModule
-) {
-  if (!allTransitiveDependencies(modules, entryPoint).has(target.source)) {
-    return false;
+  if (!condition([entryPoint])) {
+    return;
   }
   const visited = new Set<string>();
-  const stack = [entryPoint];
-
+  const stack: ModuleInfo[][] = [[entryPoint]];
   while (stack.length > 0) {
-    const current = stack.pop()!;
-    if (current.source === target.source) {
-      return true;
-    }
-    if (current.source === avoid.source || visited.has(current.source)) {
-      continue;
-    }
-    visited.add(current.source);
-
-    for (const dep of current.dependencies) {
-      const depModule = findModule(modules, dep.resolved);
-      if (depModule != null) {
-        stack.push(depModule);
+    const path = stack.pop()!;
+    const current = path[path.length - 1];
+    visited.add(current.id);
+    yield current;
+    for (const dep of current.importedIds) {
+      if (visited.has(dep)) continue;
+      const depModule = findModule(modules, dep);
+      if (depModule == null) throw new Error(`Module ${dep} not found`);
+      const newPath = [...path, depModule];
+      if (condition(newPath)) {
+        stack.push(newPath);
       }
     }
   }
-
-  return false;
 }
 
-declare module "dependency-cruiser" {
-  interface IModule {
-    minifiedSize: number;
+function uniqueSize(
+  modules: ModuleInfo[],
+  entryPoint: ModuleInfo,
+  path: ModuleInfo[]
+) {
+  const cacheKey = `${entryPoint.id}\0${path.map((m) => m.id).join("\0")}`;
+  if (uniqueSizeCache.has(cacheKey)) {
+    return uniqueSizeCache.get(cacheKey)!;
   }
+  const reachableFromChokePoint = new Set(
+    reachableFrom(modules, path[path.length - 1])
+  );
+  for (const module of reachableFrom(
+    modules,
+    entryPoint,
+    (p) => !(p.length === path.length && p.every((m, i) => m.id === path[i].id))
+  )) {
+    reachableFromChokePoint.delete(module);
+  }
+  let total = 0;
+  for (const module of reachableFromChokePoint) {
+    total += module.size;
+  }
+  uniqueSizeCache.set(cacheKey, total);
+  return total;
 }
